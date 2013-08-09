ecfc28415d00186f6fa6cc788fc7f5d92e1d8bed
Beza Getachew <bezagetachew921@yahoo.com>
2013-08-09 16:06:59 +0300

    This is my firtst commit

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :name, :price, :released_at, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Widget::Application.config.secret_key_base = 'ae2b9a586fc91997e54e92b89d8d67c926c8e702cce1539bf1bb99abad290065868cb5d626d1b847706cd83432d6f38332758282be95df48f1944ee51141b5fa'
